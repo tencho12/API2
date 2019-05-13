1a0125ee03ed0aa474d004b6424d92a3304b7041
tencho12 <tenzincho74@gmail.com>
2019-05-13 21:12:53 +0600

    changes

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cors = require('cors')
 const bcrypt = require('bcrypt')
 const jwt= require('jsonwebtoken')
 //Setting port number
-const port = process.env.PORT || 619;
+const port = process.env.PORT || 47;
 
 process.env.SECRET_KEY = 'secret'
 
@@ -76,52 +76,102 @@ app.post('/updateStatus', function (req, res) {
 app.post('/login', function (req, res) {
     var sql = "SELECT * from user_tb where email=? LIMIT 1";
     var value = req.body.email;
-    var userid;
     mysqlConnection.query(sql, value, (err, rows, fields) => {
-        var password = rows[0].password;
-        
-        var userData={}
-        userData['email'] = rows[0].email
-        userData['password'] = rows[0].password
-        userData['user_name'] = rows[0].user_name
-        userData['location'] = rows[0].location
-        userData['house_number'] = rows[0].house_number
-        
-        if (rows.length) {
-            if (bcrypt.compareSync(req.body.password, password)) {
-                let token = jwt.sign(userData, process.env.SECRET_KEY, {
-                    expiresIn: '1440h'
-                })
-                res.send(token)
+        if (rows.length>0 && !null) {
+            var password = rows[0].password;
+            var userData = {}
+            userData['user_id'] = rows[0].user_id
+            userData['email'] = rows[0].email
+            userData['password'] = rows[0].password
+            userData['user_name'] = rows[0].user_name
+            userData['location'] = rows[0].location
+            userData['house_number'] = rows[0].house_number
+            if (rows.length) {
+                if (bcrypt.compareSync(req.body.password, password)) {
+                    let token = jwt.sign(userData, process.env.SECRET_KEY, {
+                        expiresIn: '1440h'
+                    })
+                    res.send(token)
+                } else {
+                    res.send({
+                        errorOccurred: 'true',
+                        error: 'Password does not match!'
+                    });
+                }
             } else {
-                res.status(400).json({ error: "User Does not exist." })
+                res.send({
+                    errorOccurred: 'true',
+                    error: 'No such user!'
+                });
             }
-        }else {
-            res.send("empty array")
-       } 
+        } else {
+            res.send({
+                errorOccurred: 'true'
+            });
+
+        }    
     });
 });
 
-app.post('/register', (req, res) => {
+// app.post('/register', (req, res) => {
     
-    let sql = "SELECT * FROM user_tb WHERE email=?";
-    let values = req.body.email;
-    mysqlConnection.query(sql, values, (err, rows, fields) => {
-        console.log(rows.length)
-        if (rows.length<0) {
-            res.send("check")
-            bcrypt.hash(req.body.password, 10, (err, hash) => {
-                var pass = hash
+//     let sql = "SELECT * FROM user_tb WHERE email=?";
+//     let values = req.body.email;
+//     mysqlConnection.query(sql, values, (err, rows, fields) => {
+//         console.log(rows.length)
+//         if (rows.length<0) {
+//             res.send("check")
+//             bcrypt.hash(req.body.password, 10, (err, hash) => {
+//                 var pass = hash
 
-                let sql = "INSERT INTO user_tb(user_name,email,password,location,house_number) VALUES (?,?,?,?,?)";
-                let values = [req.body.user_name, req.body.email, pass, req.body.location, req.body.house_number];
-                mysqlConnection.query(sql, values, (err, rows, fields) => { 
-                    res.send(rows)
-                })
-            })
+//                 let sql = "INSERT INTO user_tb(user_name,email,password,location,house_number) VALUES (?,?,?,?,?)";
+//                 let values = [req.body.user_name, req.body.email, pass, req.body.location, req.body.house_number];
+//                 mysqlConnection.query(sql, values, (err, rows, fields) => { 
+//                     res.send(rows)
+//                 })
+//             })
+//         } else {
+//              res.json({ error: "user already exist" })
+//         }
+//     })
+// });
+
+app.post('/updateProfile', function (req, res) {
+
+    var sql = "SELECT password from user_tb WHERE user_id=?";
+    var value = req.body.user_id;
+    mysqlConnection.query(sql, value, (err, rows, fields) => {
+        if (rows.length) {
+            var cpass = rows[0].password
+            if (bcrypt.compareSync(req.body.cpassword, cpass)) {
+                var sql = "UPDATE user_tb SET user_name=?, email=?, password=?, location=?, house_number=? WHERE user_id=?";
+                var password;
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    password = hash
+                    var value = [req.body.user_name, req.body.email, password, req.body.location, req.body.house_number, req.body.user_id]
+                    mysqlConnection.query(sql, value, (err, rows, fields) => {
+                        if (rows) {
+                            res.send(rows)
+                        } else {
+                            res.send({
+                                errorOccurred: 'true',
+                                error: 'Something is wrong'
+                            });
+                        }
+                    });
+                });
+            } else {
+                res.send({
+                    errorOccurred: 'true',
+                    error:'Password does not match!'
+                });
+            }
+            
         } else {
-            res.send(rows.length)
-            // res.json({ error: "user already exist" })
+            res.send({
+                errorOccurred: 'true',
+                error: 'No such user found!'
+            });
         }
-    })
+    });
 });
